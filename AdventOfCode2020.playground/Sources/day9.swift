8a532d00cac95a294f883efc55450f30771428f8
Jens Bruggemans <jens.bruggemans@persgroep.be>
2020-12-09 12:28:51 +0100

    day 9

---
+++ AdventOfCode2020.playground/Sources/day9.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public func day9() {
+    let fileURL = Bundle.main.url(forResource: "day9", withExtension: "txt")
+    let content: String = try! String(contentsOf: fileURL!, encoding: String.Encoding.utf8).trimmingCharacters(in: CharacterSet.newlines)
+    let numbers = content.components(separatedBy: "\n").map{ Int($0)! }
+    
+    let result1 = numbers[25..<numbers.count].enumerated().first(where: {
+        !Array(numbers[$0..<25 + $0]).canAddTo($1)
+    })!.1
+    print("Day 9 part 1 result: \(result1)")
+   
+    let array = numbers.subArrayAddingUpTo(result1)!
+    let result2 = array.min()! + array.max()!
+    print("Day 9 part 2 result: \(result2)")
+}
+
+extension Array where Element == Int {
+    func canAddTo(_ sum: Int) -> Bool {
+        return self.enumerated().first(where: { self[($0 + 1)..<self.count].contains(sum - $1) }) != nil
+    }
+    
+    func subArrayAddingUpTo(_ sum: Int) -> [Int]? {
+        var startIndex = 0
+        var endIndex = 0
+        while startIndex < self.count && endIndex < self.count {
+            let subArray = self[startIndex..<endIndex]
+            let subSum = subArray.reduce(0, { $0 + $1 })
+            if subSum > sum {
+                startIndex += 1
+            } else if subSum < sum {
+                endIndex += 1
+            } else {
+                return Array(subArray)
+            }
+        }
+        return nil
+    }
+}
